50753f2d59bd655d8ba7cbee8ee899700ddba7aa
lvichi <lvichi@student.42porto.com>
2023-10-03 20:41:52 +0100

    A

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,31 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lvichi <lvichi@student.42porto.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/03 15:31:57 by lvichi            #+#    #+#             */
-/*   Updated: 2023/10/03 20:40:47 by lvichi           ###   ########.fr       */
+/*   Created: 2023/10/03 20:22:35 by lvichi            #+#    #+#             */
+/*   Updated: 2023/10/03 20:40:09 by lvichi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "libft.h"
 
-int	main(void)
+size_t	ft_strlen(char *str)
 {
-	char	c;
-	char	*str;
+	size_t	i;
 
-	c = ' ';
-	str = "Leonardo Vichi";
-	printf("Char : %c\n", c);
-	printf("ft_isalpha : %d\n", ft_isalpha(c));
-	printf("ft_isdigit : %d\n", ft_isdigit(c));
-	printf("ft_isalnum : %d\n", ft_isalnum(c));
-	printf("ft_isascii : %d\n", ft_isascii(c));
-	printf("ft_isprint : %d\n", ft_isprint(c));
-	printf("ft_strlen : %zu\n", ft_strlen(str));
-	return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
--- libft.h
+++ libft.h
@@ -6,18 +6,20 @@
 /*   By: lvichi <lvichi@student.42porto.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 17:29:12 by lvichi            #+#    #+#             */
-/*   Updated: 2023/10/03 20:00:43 by lvichi           ###   ########.fr       */
+/*   Updated: 2023/10/03 20:35:11 by lvichi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 
 # define LIBFT_H
+# include <stdlib.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(char *str);
 
 #endif
