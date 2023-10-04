b743c2cb45028b817afae1ccf1869ea9796d10c2
lvichi <lvichi@student.42porto.com>
2023-10-04 15:55:21 +0100

    A

--- ft_memset.c
+++ ft_memset.c
@@ -1,26 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lvichi <lvichi@student.42porto.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/03 17:29:12 by lvichi            #+#    #+#             */
-/*   Updated: 2023/10/04 15:55:16 by lvichi           ###   ########.fr       */
+/*   Created: 2023/10/03 20:54:26 by lvichi            #+#    #+#             */
+/*   Updated: 2023/10/04 15:55:14 by lvichi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
+#include "libft.h"
 
-# define LIBFT_H
-# include <stdlib.h>
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(char *str);
-void	*ft_memset(void *s, int c, size_t n);
-
-#endif
+void	*ft_memset(void *s, int c, size_t n)
+{
+	while (n)
+		((char *)s)[--n] = c;
+	return (s);
+}
--- main.c
+++ main.c
@@ -6,26 +6,31 @@
 /*   By: lvichi <lvichi@student.42porto.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 15:31:57 by lvichi            #+#    #+#             */
-/*   Updated: 2023/10/03 20:40:47 by lvichi           ###   ########.fr       */
+/*   Updated: 2023/10/04 15:55:13 by lvichi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
+#include <unistd.h>
 
 int	main(void)
 {
 	char	c;
-	char	*str;
+	char	str[30] = "Leonardo Vichi";
 
 	c = ' ';
-	str = "Leonardo Vichi";
 	printf("Char : %c\n", c);
 	printf("ft_isalpha : %d\n", ft_isalpha(c));
 	printf("ft_isdigit : %d\n", ft_isdigit(c));
 	printf("ft_isalnum : %d\n", ft_isalnum(c));
 	printf("ft_isascii : %d\n", ft_isascii(c));
 	printf("ft_isprint : %d\n", ft_isprint(c));
+	printf("%s\n", str);
 	printf("ft_strlen : %zu\n", ft_strlen(str));
+
+	printf("%s\n", str);
+	printf("ft_memset : %s\n", (char *)ft_memset(str, 'X', 5));
+	printf("\nEnd\n");
 	return (0);
 }
