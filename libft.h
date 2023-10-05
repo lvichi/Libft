29ffe01261c8375b6cafc3d9501c0be4e0efc62e
lvichi <lvichi@student.42porto.com>
2023-10-05 19:39:54 +0100

    Added ft_strlcpy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lvichi <lvichi@student.42porto.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 17:29:12 by lvichi            #+#    #+#             */
-/*   Updated: 2023/10/05 15:35:52 by lvichi           ###   ########.fr       */
+/*   Updated: 2023/10/05 18:09:21 by lvichi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 
 #endif
