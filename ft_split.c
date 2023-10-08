701c556b2bcb17e2ef5098129c78f0c7bdae4112
lvichi <lvichi@student.42porto.com>
2023-10-08 17:40:10 +0100

    Added function ft_spilt

--- ft_split.c
+++ ft_split.c
@@ -6,45 +6,34 @@
 /*   By: lvichi <lvichi@student.42porto.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 19:28:20 by lvichi            #+#    #+#             */
-/*   Updated: 2023/10/07 21:06:23 by lvichi           ###   ########.fr       */
+/*   Updated: 2023/10/08 17:39:32 by lvichi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-char	*ft_trim(char const *s, char c)
-{
-	char	*trim_s;
-	char	sep[1];
-	
-	sep[0] = c;
-	trim_s = ft_strtrim(s, sep);
-	return (trim_s);
-}
-
-size_t	ft_count_words(char const *trim_s, char c)
+size_t	ft_count_words(char const *s, char c)
 {
 	size_t	words;
 	size_t	i;
 	size_t	word_flag;
-	
+
 	words = 0;
 	i = 0;
-	word_flag = 1;
-	if (trim_s[i])
-		words = 1;
-	while (trim_s[i])
+	word_flag = 0;
+	while (s[i])
 	{
-		if (trim_s[i] != c)
+		if (s[i] != c)
 			word_flag = 1;
-		if (trim_s[i] == c && word_flag)
+		if (s[i] == c && word_flag)
 		{
 			words++;
 			word_flag = 0;
 		}
 		i++;
 	}
+	if (word_flag)
+		words++;
 	return (words);
 }
 
@@ -57,8 +46,10 @@ size_t	*ft_get_size(char const *trim_s, size_t w_count, char c)
 
 	words = 0;
 	i = 0;
-	word_flag = 1;
+	word_flag = 0;
 	w_size = (size_t *)malloc(sizeof(size_t) * w_count);
+	while (w_count--)
+		w_size[w_count] = 0;
 	while (trim_s[i])
 	{
 		if (trim_s[i] != c)
@@ -66,12 +57,11 @@ size_t	*ft_get_size(char const *trim_s, size_t w_count, char c)
 			word_flag = 1;
 			w_size[words]++;
 		}
-		if (trim_s[i] == c && word_flag)
+		if (trim_s[i++] == c && word_flag)
 		{
 			words++;
 			word_flag = 0;
 		}
-		i++;
 	}
 	return (w_size);
 }
@@ -84,7 +74,7 @@ char	*ft_get_word(char const *trim_s, size_t index, size_t *w_size, char c)
 	size_t	t;
 	size_t	words;
 
-	word_flag = 1;
+	word_flag = 0;
 	i = 0;
 	t = 0;
 	words = 0;
@@ -94,48 +84,54 @@ char	*ft_get_word(char const *trim_s, size_t index, size_t *w_size, char c)
 		if (trim_s[i] != c)
 			word_flag = 1;
 		if (trim_s[i] != c && words == index)
-			word[t++] = trim_s[i];  
-		if (trim_s[i] == c && word_flag)
+			word[t++] = trim_s[i];
+		if (trim_s[i++] == c && word_flag)
 		{
 			words++;
 			word_flag = 0;
 		}
-		i++;
 	}
+	word[t] = 0;
 	return (word);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	*trim_s;
 	char	**words;
 	size_t	w_count;
 	size_t	i;
 	size_t	*w_size;
 
 	i = 0;
-	trim_s = ft_trim(s, c);
-	w_count = ft_count_words(trim_s, c);
+	w_count = ft_count_words(s, c);
 	words = (char **)malloc(sizeof(char *) * (w_count + 1));
-	w_size = ft_get_size(trim_s, w_count, c); 
+	if (!words)
+		return (NULL);
+	w_size = ft_get_size(s, w_count, c); 
+	if (!w_size)
+		return (NULL);
 	while (i < w_count)
 	{
-		words[i] = ft_get_word(trim_s, i, w_size, c);
+		words[i] = ft_get_word(s, i, w_size, c);
+		if (!words[i])
+			return (NULL);
 		i++;
 	}
 	words[i] = 0;
-	i = 0;
 	free(w_size);
-	free(trim_s);
 	return (words);
 }
 
+/*#include <stdio.h>
+
 int	main(void)
 {
-	char	s[50] = "...rat..phanter.duck...hamster.";
-	char	c = '.';
+	char	*s = "                  olol";
+	char	c = ' ';
 	char	**words;
-	
+	size_t	i;
+
+	i = 0;
 	words = ft_split(s, c);
 	while (words[i])
 	{
@@ -144,4 +140,4 @@ int	main(void)
 		i++;
 	}
 	free(words);
-}
+}*/
