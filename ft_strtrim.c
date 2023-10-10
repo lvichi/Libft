57630548dd26eff63434daab4dda3d707d474698
lvichi <lvichi@student.42porto.com>
2023-10-10 17:16:28 +0100

    Fixed memory leak on ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,87 +6,77 @@
 /*   By: lvichi <lvichi@student.42porto.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 15:11:28 by lvichi            #+#    #+#             */
-/*   Updated: 2023/10/07 19:25:07 by lvichi           ###   ########.fr       */
+/*   Updated: 2023/10/10 17:08:58 by lvichi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_pos(char const *s1, char const *set)
+int	ft_begin(char const *s1, char const *set)
 {
-	int	begin_flag;
 	int	i;
 	int	t;
+	int	flag;
 
-	begin_flag = 1;
 	i = -1;
-	while (s1[++i] && begin_flag)
+	while (s1[++i])
 	{
-		t = 0;
-		while (set[t])
-		{
-			begin_flag = 0;
+		t = -1;
+		flag = 1;
+		while (set[++t])
 			if (s1[i] == set[t])
-			{
-				begin_flag = 1;
-				break ;
-			}
-			t++;
-		}
-		if (!begin_flag)
-			return ((size_t)i);
+				flag = 0;
+		if (flag)
+			break ;
 	}
-	if (!s1[i])
-		return ((size_t)i);
-	return (0);
+	return (i);
 }
 
-size_t	ft_end(char const *s1, char const *set)
+int	ft_end(char const *s1, char const *set)
 {
-	char	*rev_s1;
-	size_t	i;
-	size_t	t;
-	size_t	end;
+	int	i;
+	int	t;
+	int	flag;
+
+	i = ft_strlen(s1);
+	while (i-- > 0)
+	{
+		t = -1;
+		flag = 1;
+		while (set[++t])
+			if (s1[i] == set[t])
+				flag = 0;
+		if (flag)
+			break ;
+	}
+	return (i + 1);
+}
+
+void	ft_fill(char const *s1, char *str, int begin, int end)
+{
+	int	i;
 
 	i = 0;
-	t = (ft_strlen(s1) - 1);
-	rev_s1 = (char *)malloc((ft_strlen(s1) + 1));
-	if (!rev_s1)
-		return (0);
-	while (i < ft_strlen(s1))
-		rev_s1[i++] = s1[t--];
-	rev_s1[i] = 0;
-	end = ft_pos(rev_s1, set);
-	free(rev_s1);
-	return (end);
+	while (begin < end)
+		str[i++] = s1[begin++];
+	str[i] = 0;
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	begin;
-	size_t	end;
-	size_t	size;
-	size_t	i;
+	int		begin;
+	int		end;
 	char	*str;
 
-	i = 0;
-	begin = ft_pos(s1, set);
+	begin = ft_begin(s1, set);
 	end = ft_end(s1, set);
-	if (begin == ft_strlen(s1) && !set[0])
-	{
-		begin = 0;
-		size = ft_strlen(s1);
-	}
-	else if (begin == ft_strlen(s1))
-		size = 0;
-	else
-		size = ft_strlen(s1) - begin - end;
-	str = (char *)malloc(sizeof(char) * (size + 1));
+	if (begin > end)
+		begin = end;
+	str = (char *)malloc(sizeof(char) * (end - begin + 1));
 	if (!str)
 		return (NULL);
-	while (i < size)
-		str[i++] = s1[begin++];
-	str[i] = 0;
+	str[0] = 0;
+	ft_fill(s1, str, begin, end);
 	return (str);
 }
 
@@ -94,20 +84,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 int	main(void)
 {
-	char	str[30] = "abcd";
-	char	*trimed_str;
+	char	str[30] = ",,..,,abcd,.t..";
+	char	*new_str;
 
-	trimed_str = ft_strtrim(str, "");
-	printf("ft_strtrim: %s\n", trimed_str);
-	free(trimed_str);
-}
-
-	printf("begin: %zu\n", begin);
-	printf("end: %zu\n", end);
-	printf("size: %zu\n", size);
-
-	if (begin == (ft_strlen(s1)) && set[0])
-		size = 0;
-	if (size > ft_strlen(s1))
-		size = ft_strlen(s1);
-*/
+	new_str = ft_strtrim(str, ".,");
+	printf("ft_strtrim: %s\n", new_str);
+	free(new_str);
+}*/
